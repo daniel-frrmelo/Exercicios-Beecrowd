2b90482330bf40e98abd08a3363f5902636afcee
Daniel Ferreira de Melo <daniel.ferreira7d@gmail.com>
2026-07-30 17:58:31 -0300

    Adicionado os primeiros 3 arquivos, equivalente aos 3 primeiros exercicios

---
+++ beecrowd-ex1000.c
@@ -0,0 +1,11 @@
+// Problema n°1000 do beecrowd
+
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Hello World!\n");
+
+    return 0;
+}
---
+++ beecrowd-ex1002.c
@@ -0,0 +1,8 @@
+// Problema n°1002 do beecrowd
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
